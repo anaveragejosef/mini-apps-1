b9286a652848797e32117c679f734c7b0785fc87
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-09 11:25:32 -0700

    Adds Pseudocode for Server.js Functions
    In order to accomplish this, I will need to create a get route that
    will then delegate to a function that creates the CSV response. Then
    this will send it back to the client.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -9,3 +9,22 @@ app.use(express.static('client'));
 app.use(express.json());
 // List on port 3000
 app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
+
+// Create route
+  // First create an express call using post
+  // The URL will be root for now
+  // Within the res/req function call a parse data function
+    // This takes the results and a callback
+    // Based on response, send error or CSV
+
+// Parse Data
+  // Accepts the JSON object
+    // Create new string
+     // Get the keys from first obj
+      // Add these as the first line
+    // Helper function
+      // Iterate through the object
+      // Create a new line with each value seperated by comma
+      // If children, call helper
+    // Start function with first object
+  // Once complete, callback with result
