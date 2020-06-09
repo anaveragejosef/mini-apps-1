919279ec013a668a4e0386fd8f13242561868fbd
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-09 11:06:40 -0700

    Creates Server.js File and Installs Express
    This commit has installed express with server.js as the entry point.
    Now we can serve the files to the client side and begin looking for
    the POST request.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+// Require and create an express instance
+const express = require('express');
+const app = express();
+const port = 3000;
 
-
+// Serve client side html and js files
+app.use(express.static('client'));
+// Use json to parse data into req.body
+app.use(express.json());
+// List on port 3000
+app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
