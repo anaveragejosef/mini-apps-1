ae23a4c45ffebb9f6c2aaf94b724e4a815c8f495
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-11 15:46:57 -0700

    Adds Beginning of Adding Pieces to Board
    This commit begins to add the logic and structure for how to add a
    piece to the board. At this point I am attempting to update the board
    in state and then I will update the color of the board.

--- challenge_4/src/GameBoard.jsx
+++ challenge_4/src/GameBoard.jsx
@@ -1,53 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var GameBoard = (props) => {
-  return (
-    <div id='game-board' class='game-board'>
-      <div class="square" data-x="0" col="0" id="r0c0" onClick={props.onClick}/>
-      <div class="square" row="1" col="0"/>
-      <div class="square" row="2" col="0"/>
-      <div class="square" row="3" col="0"/>
-      <div class="square" row="4" col="0"/>
-      <div class="square" row="5" col="0"/>
-      <div class="square" row="0" col="1"/>
-      <div class="square" row="1" col="1"/>
-      <div class="square" row="2" col="1"/>
-      <div class="square" row="3" col="1"/>
-      <div class="square" row="4" col="1"/>
-      <div class="square" row="5" col="1"/>
-      <div class="square" row="0" col="2"/>
-      <div class="square" row="1" col="2"/>
-      <div class="square" row="2" col="2"/>
-      <div class="square" row="3" col="2"/>
-      <div class="square" row="4" col="2"/>
-      <div class="square" row="5" col="2"/>
-      <div class="square" row="0" col="3"/>
-      <div class="square" row="1" col="3"/>
-      <div class="square" row="2" col="3"/>
-      <div class="square" row="3" col="3"/>
-      <div class="square" row="4" col="3"/>
-      <div class="square" row="5" col="3"/>
-      <div class="square" row="0" col="4"/>
-      <div class="square" row="1" col="4"/>
-      <div class="square" row="2" col="4"/>
-      <div class="square" row="3" col="4"/>
-      <div class="square" row="4" col="4"/>
-      <div class="square" row="5" col="4"/>
-      <div class="square" row="0" col="5"/>
-      <div class="square" row="1" col="5"/>
-      <div class="square" row="2" col="5"/>
-      <div class="square" row="3" col="5"/>
-      <div class="square" row="4" col="5"/>
-      <div class="square" row="5" col="5"/>
-      <div class="square" row="0" col="6"/>
-      <div class="square" row="1" col="6"/>
-      <div class="square" row="2" col="6"/>
-      <div class="square" row="3" col="6"/>
-      <div class="square" row="4" col="6"/>
-      <div class="square" row="5" col="6"/>
-    </div>
-  );
+class GameBoard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+      ],
+      currentPlayer: 1
+    }
+    this.placePiece = this.placePiece.bind(this);
+  }
+
+  placePiece(id) {
+    let col;
+    id = id.split('');
+    if (id[0] === 'a') {
+      col = 0;
+    }
+    // Create var for board column
+    var boardCol = this.state.board[col];
+    // for loop through board at [id] - iterate backwards
+    for(let i = boardCol.length - 1; i > -1; i--) {
+      // If it is 0, place turn
+      if (boardCol[i] === 0) {
+        this.setState({
+
+        })
+      }
+      // Once turn placed
+      // Update current player
+    }
+  }
+
+  render () {
+    return (
+      <div id='game-board' class='game-board'>
+        <div class="square" data-x="0" data-y="0" id="a1" onClick={(e) => this.placePiece(e.target.id)}/>
+        <div class="square" data-x="1" data-y="0"/>
+        <div class="square" data-x="2" data-y="0"/>
+        <div class="square" data-x="3" data-y="0"/>
+        <div class="square" data-x="4" data-y="0"/>
+        <div class="square" data-x="5" data-y="0"/>
+        <div class="square" data-x="0" data-y="1"/>
+        <div class="square" data-x="1" data-y="1"/>
+        <div class="square" data-x="2" data-y="1"/>
+        <div class="square" data-x="3" data-y="1"/>
+        <div class="square" data-x="4" data-y="1"/>
+        <div class="square" data-x="5" data-y="1"/>
+        <div class="square" data-x="0" data-y="2"/>
+        <div class="square" data-x="1" data-y="2"/>
+        <div class="square" data-x="2" data-y="2"/>
+        <div class="square" data-x="3" data-y="2"/>
+        <div class="square" data-x="4" data-y="2"/>
+        <div class="square" data-x="5" data-y="2"/>
+        <div class="square" data-x="0" data-y="3"/>
+        <div class="square" data-x="1" data-y="3"/>
+        <div class="square" data-x="2" data-y="3"/>
+        <div class="square" data-x="3" data-y="3"/>
+        <div class="square" data-x="4" data-y="3"/>
+        <div class="square" data-x="5" data-y="3"/>
+        <div class="square" data-x="0" data-y="4"/>
+        <div class="square" data-x="1" data-y="4"/>
+        <div class="square" data-x="2" data-y="4"/>
+        <div class="square" data-x="3" data-y="4"/>
+        <div class="square" data-x="4" data-y="4"/>
+        <div class="square" data-x="5" data-y="4"/>
+        <div class="square" data-x="0" data-y="5"/>
+        <div class="square" data-x="1" data-y="5"/>
+        <div class="square" data-x="2" data-y="5"/>
+        <div class="square" data-x="3" data-y="5"/>
+        <div class="square" data-x="4" data-y="5"/>
+        <div class="square" data-x="5" data-y="5"/>
+        <div class="square" data-x="0" data-y="6"/>
+        <div class="square" data-x="1" data-y="6"/>
+        <div class="square" data-x="2" data-y="6"/>
+        <div class="square" data-x="3" data-y="6"/>
+        <div class="square" data-x="4" data-y="6"/>
+        <div class="square" data-x="5" data-y="6"/>
+      </div>
+    );
+  }
 }
 
 export default GameBoard;
--- challenge_4/src/app.jsx
+++ challenge_4/src/app.jsx
@@ -2,33 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameBoard from './GameBoard.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // State for turn
-    // State for piece postitions?
-    this.placePiece = this.placePiece.bind(this);
-  }
-
-  placePiece(event) {
-    console.log(event.dataset);
-    console.log(event);
-    var board = document.getElementsByClassName('square');
-    console.log(board);
-  }
-
-  render() {
-    return (
+var App = (props) => {
+  return (
+    <div>
       <div>
-        <div>
-          <h1>Let's Play Connect Four</h1>
-        </div>
-        <div>
-          <GameBoard onClick={this.placePiece}/>
-        </div>
+        <h1>Let's Play Connect Four</h1>
       </div>
-    );
-  }
+      <div>
+        <GameBoard />
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
