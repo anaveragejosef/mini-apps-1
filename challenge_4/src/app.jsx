85eff11e64ca92a1d81d02a4987ccf947c5fc3c6
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-11 10:59:22 -0700

    Formats Game Board on Home Screen
    The screen now presents the game board and has rough CSS styling to
    make it look like the Connect Four board.

--- challenge_4/src/app.jsx
+++ challenge_4/src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -9,51 +10,57 @@ class App extends React.Component {
 
   render() {
     return (
-      <div id='game-board'>
-        <div row="0" col="0"></div>
-        <div row="0" col="1"></div>
-        <div row="0" col="2"></div>
-        <div row="0" col="3"></div>
-        <div row="0" col="4"></div>
-        <div row="0" col="5"></div>
-        <div row="0" col="6"></div>
-        <div row="1" col="0"></div>
-        <div row="1" col="1"></div>
-        <div row="1" col="2"></div>
-        <div row="1" col="3"></div>
-        <div row="1" col="4"></div>
-        <div row="1" col="5"></div>
-        <div row="1" col="6"></div>
-        <div row="2" col="0"></div>
-        <div row="2" col="1"></div>
-        <div row="2" col="2"></div>
-        <div row="2" col="3"></div>
-        <div row="2" col="4"></div>
-        <div row="2" col="5"></div>
-        <div row="2" col="6"></div>
-        <div row="3" col="0"></div>
-        <div row="3" col="1"></div>
-        <div row="3" col="2"></div>
-        <div row="3" col="3"></div>
-        <div row="3" col="4"></div>
-        <div row="3" col="5"></div>
-        <div row="3" col="6"></div>
-        <div row="4" col="0"></div>
-        <div row="4" col="1"></div>
-        <div row="4" col="2"></div>
-        <div row="4" col="3"></div>
-        <div row="4" col="4"></div>
-        <div row="4" col="5"></div>
-        <div row="4" col="6"></div>
-        <div row="5" col="0"></div>
-        <div row="5" col="1"></div>
-        <div row="5" col="2"></div>
-        <div row="5" col="3"></div>
-        <div row="5" col="4"></div>
-        <div row="5" col="5"></div>
-        <div row="5" col="6"></div>
+      <div>
+        <div>
+          <h1>Let's Play Connect Four</h1>
+        </div>
+        <div id='game-board'>
+          <div class="square" row="0" col="0"/>
+          <div class="square" row="0" col="1"/>
+          <div class="square" row="0" col="2"/>
+          <div class="square" row="0" col="3"/>
+          <div class="square" row="0" col="4"/>
+          <div class="square" row="0" col="5"/>
+          <div class="square" row="0" col="6"/>
+          <div class="square" row="1" col="0"/>
+          <div class="square" row="1" col="1"/>
+          <div class="square" row="1" col="2"/>
+          <div class="square" row="1" col="3"/>
+          <div class="square" row="1" col="4"/>
+          <div class="square" row="1" col="5"/>
+          <div class="square" row="1" col="6"/>
+          <div class="square" row="2" col="0"/>
+          <div class="square" row="2" col="1"/>
+          <div class="square" row="2" col="2"/>
+          <div class="square" row="2" col="3"/>
+          <div class="square" row="2" col="4"/>
+          <div class="square" row="2" col="5"/>
+          <div class="square" row="2" col="6"/>
+          <div class="square" row="3" col="0"/>
+          <div class="square" row="3" col="1"/>
+          <div class="square" row="3" col="2"/>
+          <div class="square" row="3" col="3"/>
+          <div class="square" row="3" col="4"/>
+          <div class="square" row="3" col="5"/>
+          <div class="square" row="3" col="6"/>
+          <div class="square" row="4" col="0"/>
+          <div class="square" row="4" col="1"/>
+          <div class="square" row="4" col="2"/>
+          <div class="square" row="4" col="3"/>
+          <div class="square" row="4" col="4"/>
+          <div class="square" row="4" col="5"/>
+          <div class="square" row="4" col="6"/>
+          <div class="square" row="5" col="0"/>
+          <div class="square" row="5" col="1"/>
+          <div class="square" row="5" col="2"/>
+          <div class="square" row="5" col="3"/>
+          <div class="square" row="5" col="4"/>
+          <div class="square" row="5" col="5"/>
+          <div class="square" row="5" col="6"/>
+        </div>
       </div>
     );
   }
 }
 
+ReactDOM.render(<App />, document.getElementById('app'));
