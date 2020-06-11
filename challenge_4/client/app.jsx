77ae541a2564cd4e2904185900dd974073d52b15
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-11 09:46:07 -0700

    Adds FrameWork for HTML and Gameboard
    This adds the needed framework for the HTML page, without scripts or
    webpack yet and then creates a basic gameboard layout using divs.

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,57 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // State for turn
+    // State for piece postitions?
+  }
 
+  render() {
+    return (
+      <div id='game-board'>
+        <div row="0" col="0"></div>
+        <div row="0" col="1"></div>
+        <div row="0" col="2"></div>
+        <div row="0" col="3"></div>
+        <div row="0" col="4"></div>
+        <div row="0" col="5"></div>
+        <div row="0" col="6"></div>
+        <div row="1" col="0"></div>
+        <div row="1" col="1"></div>
+        <div row="1" col="2"></div>
+        <div row="1" col="3"></div>
+        <div row="1" col="4"></div>
+        <div row="1" col="5"></div>
+        <div row="1" col="6"></div>
+        <div row="2" col="0"></div>
+        <div row="2" col="1"></div>
+        <div row="2" col="2"></div>
+        <div row="2" col="3"></div>
+        <div row="2" col="4"></div>
+        <div row="2" col="5"></div>
+        <div row="2" col="6"></div>
+        <div row="3" col="0"></div>
+        <div row="3" col="1"></div>
+        <div row="3" col="2"></div>
+        <div row="3" col="3"></div>
+        <div row="3" col="4"></div>
+        <div row="3" col="5"></div>
+        <div row="3" col="6"></div>
+        <div row="4" col="0"></div>
+        <div row="4" col="1"></div>
+        <div row="4" col="2"></div>
+        <div row="4" col="3"></div>
+        <div row="4" col="4"></div>
+        <div row="4" col="5"></div>
+        <div row="4" col="6"></div>
+        <div row="5" col="0"></div>
+        <div row="5" col="1"></div>
+        <div row="5" col="2"></div>
+        <div row="5" col="3"></div>
+        <div row="5" col="4"></div>
+        <div row="5" col="5"></div>
+        <div row="5" col="6"></div>
+      </div>
+    );
+  }
+}
 
